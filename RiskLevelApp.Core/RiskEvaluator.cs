73707e7385964f82592c141e792389741f0a6216
ghazal <ghazlsaied05@gmail.com>
2025-06-20 12:26:17 +0200

    Add tests for RiskEvaluator after refactoring

--- RiskLevelApp.Core/RiskEvaluator.cs
+++ RiskLevelApp.Core/RiskEvaluator.cs
@@ -1,36 +1,30 @@
 ﻿namespace RiskLevelApp.Core;
+
 public static class RiskEvaluator
 {
-    static string GetRiskLevel(int age, bool smoker, bool hasCondition)
+    public static string GetRiskLevel(int age, bool smoker, bool hasCondition)
     {
-        if (age < 18)
+        if (IsChild(age))
             return "Low";
-        if (age >= 18 && age < 40)
-        {
-            if (smoker)
-            {
-                if (hasCondition)
-                    return "Medium";
-                else
-                    return "Low";
-            }
-            else
-            {
-                if (hasCondition)
-                    return "Medium";
-                else
-                    return "Low";
-            }
-        }
-        if (age >= 40)
-        {
-            if (smoker && hasCondition)
-                return "High";
-            else if (smoker || hasCondition)
-                return "Medium";
-            else
-                return "Low";
-        }
+        if (IsYoungAdult(age))
+            return EvaluateYoungAdult(hasCondition);
+        if (IsOlder(age))
+            return EvaluateOlder(smoker, hasCondition);
         return "Unknown";
+    }
+    private static bool IsChild(int age) => age > 0 && age < 18;
+    private static bool IsYoungAdult(int age) => age >= 18 && age < 40;
+    private static bool IsOlder(int age) => age >= 40;
+    private static string EvaluateYoungAdult(bool hasCondition)
+    {
+        return (hasCondition) ? "Medium" : "Low";
+    }
+    private static string EvaluateOlder(bool smoker, bool hasCondition)
+    {
+        if (smoker && hasCondition)
+            return "High";
+        if (smoker || hasCondition)
+            return "Medium";
+        return "Low";
     }
 }
--- RiskLevelApp.Tests/obj/Debug/net9.0/RiskLevelApp.Tests.AssemblyInfo.cs
+++ RiskLevelApp.Tests/obj/Debug/net9.0/RiskLevelApp.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RiskLevelApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcdeff09e296320abeefd5e65a439ae0b4471522")]
 [assembly: System.Reflection.AssemblyProductAttribute("RiskLevelApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RiskLevelApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
